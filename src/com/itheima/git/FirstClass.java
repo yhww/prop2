bb10bb24ebe100b7b7186652da696b389ef945bc
wyh <wyh@cn>
2018-09-11 18:31:14 +0800

    第二次修改FirstClass

--- src/com/itheima/git/FirstClass.java
+++ src/com/itheima/git/FirstClass.java
@@ -3,5 +3,8 @@ package com.itheima.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("git我汤浩来啦!");
+        System.out.println("1111111111");
+        System.out.println("1111111111");
+        System.out.println("1111111111");
     }
 }
